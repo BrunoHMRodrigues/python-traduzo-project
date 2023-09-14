66875f07fd47b64c9d2b7295027284ea08e1ba23
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-09-14 18:30:34 -0300

    Feature: Requisite 05 Completed

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,17 +1,49 @@
-from flask import Blueprint, render_template
-# from deep_translator import GoogleTranslator
+from flask import Blueprint, render_template, request
+from deep_translator import GoogleTranslator
 from models.language_model import LanguageModel
 # from models.history_model import HistoryModel
 
 
 translate_controller = Blueprint("translate_controller", __name__)
 
+DEFAULT_TEXT_TO_TRANSLATE = "O que deseja traduzir"
+DEFAULT_TRANSLATE_FROM = "pt"
+DEFAULT_TRANSLATE_TO = "en"
+DEFAULT_TRANSLATED = "Tradução"
 
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
     languages = LanguageModel.list_dicts()
 
+    if request.method == "POST":
+        text_to_translate = request.form.get("text-to-translate")
+        translate_from = request.form.get("translate-from")
+        translate_to = request.form.get("translate-to")
+
+        try:
+            translator = GoogleTranslator(source=translate_from, target=translate_to)
+            translated = translator.translate(text_to_translate)
+
+        except Exception as e:
+            # Lida com possíveis erros na tradução, por exemplo, se o serviço não estiver disponível
+            return render_template(
+                "index.html",
+                languages=languages,
+                text_to_translate=text_to_translate,
+                translate_from=translate_from,
+                translate_to=translate_to,
+                translated="Erro na tradução: " + str(e)
+            )
+
+        return render_template(
+            "index.html",
+            languages=languages,
+            text_to_translate=text_to_translate,
+            translate_from=translate_from,
+            translate_to=translate_to,
+            translated=translated
+        )
 
     text_to_translate = "O que deseja traduzir"
     translate_from = "pt"
@@ -21,10 +53,10 @@ def index():
     return render_template(
             "index.html",
             languages=languages,
-            text_to_translate=text_to_translate,
-            translate_from=translate_from,
-            translate_to=translate_to,
-            translated=translated
+            text_to_translate=DEFAULT_TEXT_TO_TRANSLATE,
+            translate_from=DEFAULT_TRANSLATE_FROM,
+            translate_to=DEFAULT_TRANSLATE_TO,
+            translated=DEFAULT_TRANSLATED
         )
 
 
