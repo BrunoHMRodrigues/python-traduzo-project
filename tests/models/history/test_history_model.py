1cb7a12bf3fa0b3d0bb42eec146ebe06b2753f7e
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-09-14 20:56:52 -0300

    Feature: Requisite 07 Completed

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,28 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+import pytest
+from src.models.history_model import HistoryModel
 
+
+data = [
+        {
+            "text_to_translate": "Hello, I like videogame",
+            "translate_from": "en",
+            "translate_to": "pt"
+        },
+        {
+            "text_to_translate": "Do you love music?",
+            "translate_from": "en",
+            "translate_to": "pt"
+        }
+    ]
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    response = HistoryModel.list_as_json()
+    response = json.loads(response)
+
+    for item in response:
+        item.pop("_id")
+    # response = [{"text_to_translate": item["text_to_translate"], "translate_from": item["translate_from"], "translate_to": item["translate_to"]} for item in response]
+
+    assert response == data
